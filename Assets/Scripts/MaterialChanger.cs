a1b270aef092c2e822376d4798e045f13fac8128
luiseduve <luiseduve@hotmail.com>
2021-01-19 20:43:27 +0100

    testing teleportation

---
+++ Assets/Scripts/MaterialChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+
+    public GameObject objectToChange;
+    public Material newMaterial;
+    private Material originalMaterial;
+    private bool currentlyOrigMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMaterial = objectToChange.GetComponent<Renderer>().materials[0];
+        currentlyOrigMaterial = true;
+    }
+
+    public void ChangeMaterial()
+    {
+        if(currentlyOrigMaterial)
+        {
+            objectToChange.GetComponent<Renderer>().material = newMaterial;
+            currentlyOrigMaterial = false;
+        }
+        else
+        {
+            objectToChange.GetComponent<Renderer>().material = originalMaterial;
+            currentlyOrigMaterial = true;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            ChangeMaterial();
+        }
+    }
+
+}
